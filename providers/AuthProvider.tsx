c5e62059388b857331263a84754540d8ae31599e
Joel Diez <joeldzv95@gmail.coim>
2020-09-17 17:23:31 -0500

    auth proveider facebook

--- providers/AuthProvider.tsx
+++ providers/AuthProvider.tsx
@@ -8,7 +8,13 @@ import Cookie from 'js-cookie'
 
 
 type typeUser = {nombre?: string, jwt?:string }
-type typeAuthContext = { user: typeUser; isAuthenticated : boolean ;  logout :  () => void, login : (data:{identifier:string, password:string, remember:boolean},urlBack:string, setModalAuthSignIn?:(_:any)=>any, setErrorMessage?:(_:any)=>any) => void };
+type typeAuthContext = { 
+    user: typeUser; 
+    isAuthenticated : boolean ;  
+    logout :  () => void;
+    login : (data:{identifier:string, password:string, remember:boolean},urlBack:string, setModalAuthSignIn?:(_:any)=>any, setErrorMessage?:(_:any)=>any) => void;
+    loginProvider : (provider)=>void
+}
 
 const AuthContext = React.createContext<typeAuthContext>({} as typeAuthContext);
 
@@ -20,8 +26,8 @@ export const AuthProvider = ({children})=>{
     //context
     const router = useRouter()
     //Effect
+    const url = process.env.NEXT_PUBLIC_URL_STRAPI
     useEffect(() => {
-        const url = process.env.NEXT_PUBLIC_URL_STRAPI
 
         if(Cookie.get('authTokenMercatto') !== undefined && user.nombre){
             axios.get(`${url}/users/me`, {
@@ -39,15 +45,14 @@ export const AuthProvider = ({children})=>{
     }, [])
 
 
-    const login = async (data:{identifier:string, password:string, remember:boolean}, urlBack:string, setModalAuthSignIn?:(_:any)=>any, setErrorMessage?:(_:any)=>any) => {
-        
+    const login = async (data:{identifier:string, password:string, remember:boolean}, urlBack:string, setModalAuthSignIn?:(_:any)=>any, setErrorMessage?:(_:any)=>any) => { 
         await axios.post(`${urlBack}/auth/local`,{
             identifier: data.identifier,
             password: data.password,
         }).then(res=>{
             console.log(res)
             if (data.remember) {
-                Cookie.set('authTokenMercatto', res.data.jwt,{expires:30})
+                Cookie.set('authTokenMercatto', res.data.jwt,{expires:1})
             }
             setUser({
                 nombre: res.data.user.nombre,
@@ -58,7 +63,17 @@ export const AuthProvider = ({children})=>{
         }).catch(err=>{
             setErrorMessage('Error al iniciar sesión, verifica tus credenciales')
         })
+        
     }
+
+    const loginProvider =  (data) =>{
+            setUser({
+                nombre: data.user.username,
+                jwt: data.jwt
+            })
+            Cookie.set('authTokenMercatto',data.jwt, {expires:30})
+    }
+
     const logout = ()=>{
         setUser({})
         localStorage.removeItem('authTokenMercatto')
@@ -73,7 +88,7 @@ export const AuthProvider = ({children})=>{
     }
 
     return (
-        <AuthContext.Provider value={{ isAuthenticated: !!user, user, logout , login}}>
+        <AuthContext.Provider value={{ isAuthenticated: !!user, user, logout , login, loginProvider}}>
             {children}
         </AuthContext.Provider>
     )
